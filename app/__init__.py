ff8381f0a3b66a4266570dfc979af9d84560a967
snaiws <lghmk2@gmail.com>
2021-07-31 18:51:24 +0900

    1

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,27 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import config
 
+CSV_FILEPATH = os.path.join(os.getcwd(), __name__, 'users.csv') 
+TMP_FILEPATH = os.path.join(os.getcwd(), __name__, 'tmp.csv') 
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    
+    from app.pages.main import main_bp
+    from app.pages.secondpage import second_bp
+    from app.pages.thirdpage import third_bp
+
+    app.register_blueprint(main_bp)
+    app.register_blueprint(user_bp, url_prefix='/api')    
+    app.register_blueprint(user_bp, url_prefix='/api')
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
